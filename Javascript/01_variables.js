cdcecbb8e3b3c8435a23d1470b46266c95dcd79c
Arbaz Shoukat <arbazkamboh342@.com>
2025-07-16 17:54:47 +0500

    variable lecture completed

---
+++ Javascript/01_variables.js
@@ -0,0 +1,15 @@
+// var userName = "Arbaz";
+let email = "Arbaz@gmail.com";
+const password = "123456";
+
+var userName = "Khalid";
+// let value;
+
+email = "Arbaz2@gmail.com";
+// userName = "Khalid";
+
+// password = "kkk";
+console.log(userName);
+console.log(email);
+console.log(password);
+// console.log(value);
---
+++ Javascript/02_data_types.js
@@ -0,0 +1,23 @@
+//  1. Primitive Data Types
+
+// String
+let password = 5;
+
+// Boolean
+let isMarried = false;
+
+// Number
+let rollNumber = 5;
+
+let res = password + rollNumber;
+
+console.log(res);
+
+// undefined
+let num1;
+
+// null
+// BigInt
+// Symbol
+
+// console.log(typeof password);
